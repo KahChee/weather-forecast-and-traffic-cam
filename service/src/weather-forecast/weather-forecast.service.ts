46d6a5115da165300ba6dafde1fd83da32211cac
KahChee <kahchee_kong@hotmail.com>
2024-02-11 12:48:31 +0800

    Refactor weather-forecast service to return a promise

--- service/src/weather-forecast/weather-forecast.service.ts
+++ service/src/weather-forecast/weather-forecast.service.ts
@@ -1,30 +1,35 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { AxiosResponse } from 'axios';
-import { Observable, map } from 'rxjs';
-import { AreaMetadata, Item, SearchDateTime, WeatherForecastResponse } from './dto';
+import { firstValueFrom } from 'rxjs';
+import { AreaMetadataResponse, Item, SearchDateTime, WeatherForecastResponse } from './dto';
 
 @Injectable()
 export class WeatherForecastService {
     constructor(private readonly httpService: HttpService) {}
 
-    getWeatherForecast(searchDateTime: SearchDateTime = ''): Observable<WeatherForecastResponse> {
+    async getWeatherForecast(searchDateTime: SearchDateTime = ''): Promise<WeatherForecastResponse> {
         let queryString = '';
         if (searchDateTime) {
             queryString = `?date_time=${searchDateTime}`;
         }
 
-		return this.httpService.get(`https://api.data.gov.sg/v1/environment/2-hour-weather-forecast${queryString}`)
-            .pipe(map(response => response.data));
+        const response = await firstValueFrom(
+            this.httpService.get(`https://api.data.gov.sg/v1/environment/2-hour-weather-forecast${queryString}`)
+        );
+        return response.data;
     }
     
-    getWeatherForecastData(searchDateTime: SearchDateTime = ''): Observable<Item[]> {
-        return this.getWeatherForecast(searchDateTime)
-            .pipe(map(response => response.items));
+    async getWeatherForecastData(searchDateTime?: SearchDateTime): Promise<Item[]> {
+        const response = await this.getWeatherForecast(searchDateTime);
+        return response.items;
     }
 
-    getAreaMetadata(searchDateTime: SearchDateTime = ''): Observable<AreaMetadata[]> {
-        return this.getWeatherForecast(searchDateTime)
-            .pipe(map(response => response.area_metadata));
+    async getAreaMetadata(searchDateTime?: SearchDateTime): Promise<AreaMetadataResponse> {
+        const response = await this.getWeatherForecast(searchDateTime);
+        const areaMetadata = {
+            timestamp: response.items[0].timestamp,
+            areas: response.area_metadata
+        };
+        return areaMetadata;
     }
 }
