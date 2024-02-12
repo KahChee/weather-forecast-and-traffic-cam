00e573a82598807fc31c16931572d8b60c167176
KahChee <kahchee_kong@hotmail.com>
2024-02-12 15:59:51 +0800

    Setup report module

--- service/src/app.module.ts
+++ service/src/app.module.ts
@@ -3,8 +3,9 @@ import { DatabaseModule } from './database/database.module';
 import { TrafficCamModule } from './traffic-cam/traffic-cam.module';
 import { WeatherForecastModule } from './weather-forecast/weather-forecast.module';
 import { SearchModule } from './search/search.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-    imports: [DatabaseModule, TrafficCamModule, WeatherForecastModule, SearchModule]
+    imports: [DatabaseModule, TrafficCamModule, WeatherForecastModule, SearchModule, ReportModule]
 })
 export class AppModule {}
---
+++ service/src/report/constant.ts
@@ -0,0 +1,4 @@
+import { Timezone } from "./dto";
+
+export const DEFAULT_TIMEZONE: Timezone = '+00:00';
+export const USER_TIMEZONE: Timezone = '+08:00';
