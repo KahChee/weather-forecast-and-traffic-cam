ecf062b9d064fb806bfb634bac3ab278180e5145
KahChee <kahchee_kong@hotmail.com>
2024-02-14 01:40:03 +0800

    Update styling

--- web/src/components/search-date-picker/index.js
+++ web/src/components/search-date-picker/index.js
@@ -1,18 +1,21 @@
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import './styles/index.css';
 
 const SearchDatePicker = ({ searchDateTime, handleOnChange }) => {
   return (
-    <DatePicker
-      placeholderText="Click to select a datetime"
-      showIcon
-      showTimeSelect
-      timeFormat="hh:mm aa"
-      timeIntervals={5}
-      dateFormat="yyyy-MM-dd, hh:mm aa"
-      selected={searchDateTime}
-      onChange={handleOnChange}
-    />
+    <div className="search-date-picker-container">
+      <DatePicker
+        placeholderText="Click to select a datetime"
+        showIcon
+        showTimeSelect
+        timeFormat="hh:mm aa"
+        timeIntervals={5}
+        dateFormat="yyyy-MM-dd, hh:mm aa"
+        selected={searchDateTime}
+        onChange={handleOnChange}
+      />
+    </div>
   );
 }
 
