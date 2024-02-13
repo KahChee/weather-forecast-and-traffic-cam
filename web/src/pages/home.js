9a1c425e47995bac195f0974cd381f45514c213e
KahChee <kahchee_kong@hotmail.com>
2024-02-13 14:55:27 +0800

    Integrate search location api

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,12 +1,25 @@
 import { useState } from 'react';
 import Layout from '../components/layout';
 import SearchDatePicker from '../components/search-date-picker';
+import fetchApi from '../utils/fetch-api';
+import formatDateTime from '../utils/format-date-time';
 
 const Home = () => {
   const [searchDateTime, setSearchDateTime] = useState('');
+  const [searchDateTimeString, setSearchDateTimeString] = useState(null);
+  const [locations, setLocations] = useState([]);
 
-  const handleDateTimeChange = (dateTime) => {
+  const handleDateTimeChange = async (dateTime) => {
     setSearchDateTime(dateTime);
+
+    const dateTimeString = formatDateTime(dateTime);
+    setSearchDateTimeString(dateTimeString);
+
+    if (dateTimeString) {
+      const queryString = `?searchDateTime=${dateTimeString}`;
+      const locationData = await fetchApi({ url: `v1/search/location${queryString}` });
+      setLocations(locationData);
+    }
   };
 
   return (
